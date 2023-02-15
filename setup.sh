21cd65177e21cc2a77caae1a32b8b6dd90504f95
heavymetalthings <78985712+heavymetalthings@users.noreply.github.com>
2023-02-16 00:05:01 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,9 @@ printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbase
 sudo dnf install \
 https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm 
 https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
-sudo dnf install htop git ranger codium freetype freeimage -y
+
+
+sudo dnf install htop git ranger codium freetype freeimage ffmpeg tcl tk-y
 
 wget -c https://github.com/Open-Cascade-SAS/OCCT/archive/refs/tags/V7_6_3.tar.gz
 tar -xf V7_6_3.tar.gz
