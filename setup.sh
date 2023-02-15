6c07d181fc01d629f79e4a030d33ecef1a4566ea
heavymetalthings <heavymetalthings@gmail.com>
2023-02-15 23:49:47 +0100

    lil change

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 sudo dnf update -y
 sudo rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
 printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h" | sudo tee -a /etc/yum.repos.d/vscodium.repo
-sudo dnf install htop git ranger kitty codium -y
+sudo dnf install htop git ranger codium freetype freeimage -y
 
 wget -c https://github.com/Open-Cascade-SAS/OCCT/archive/refs/tags/V7_6_3.tar.gz
 tar -xf V7_6_3.tar.gz
